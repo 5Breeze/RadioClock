a7feae3b69c943b51b4db3ac5ab68b710a658e08
bitshen <89394068+5Breeze@users.noreply.github.com>
2025-12-25 15:59:55 +0800

    Update RadioClock.ino

--- RadioClock.ino
+++ RadioClock.ino
@@ -11,7 +11,6 @@
 // - JJY (Japan): https://ja.wikipedia.org/wiki/JJY
 // - WWVB (US): https://en.wikipedia.org/wiki/WWVB
 // - DCF77 (Germany): https://www.eecis.udel.edu/~mills/ntp/dcf77.html
-// - BSF (Taiwan): https://en.wikipedia.org/wiki/BSF_(time_service)
 // - MSF (UK): https://en.wikipedia.org/wiki/Time_from_NPL_(MSF)
 // - BPC (China): https://harmonyos.51cto.com/posts/1731
 //
